14c1d48fa68b53768a88e5482c1b537ac07ece7a
Darshil122 <darshildabhi2005@gmail.com>
2024-05-21 10:48:13 +0530

    Update file

--- script.js
+++ script.js
@@ -5,29 +5,40 @@ const btns = document.querySelectorAll(".b");
 const toggle = document.querySelector('.icon');
 const navMenu = document.querySelector('.nav-menu');
 
+let modal = document.getElementById("myModal");
+let btn = document.getElementById("myBtn");
+let span = document.getElementById("close");
+
 let home = document.getElementById("home");
 let counter = document.getElementById("counter");
-let modal = document.getElementById("modal");
+let showModal = document.getElementById("showModal");
 let review = document.getElementById("review");
+
 let showCounter = document.getElementById("showCounter");
+let mymodal = document.getElementById("box");
 
 counter.onclick = function(){
+    mymodal.style.display = "none";
     showCounter.style.display = "block";
 }
 home.onclick = function(){
     showCounter.style.display = "none";
 }
-modal.onclick = function(){
+showModal.onclick = function(){
     showCounter.style.display = "none";
+    mymodal.style.display = "block";
 }
 review.onclick = function(){
     showCounter.style.display = "none";
+    mymodal.style.display = "none";
+
 }
 
 toggle.addEventListener('click', () => {
 navMenu.classList.toggle('hide');
 });
 
+//counter
 btns.forEach(function (btn){
     btn.addEventListener("click", function (e) {
     const style = e.currentTarget.classList;
@@ -49,3 +60,18 @@ btns.forEach(function (btn){
     value.textContent = count;
     });
 });
+
+//Modal
+btn.onclick = function () {
+    modal.style.display = "block";
+}
+
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
